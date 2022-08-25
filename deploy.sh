f15a398885f15e684356594e2c61c1bb5fc69506
r.ushakov <r.ushakov@xsolla.com>
2022-08-26 00:05:01 +0600

    deploy for runner??

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,11 @@ mv frontend/dist prod/f-deploy
 
 rm -rf backend/target
 
-tar -zcvf prod/deploy.tar.gz -C prod deploy
-tar -zcvf prod/f-deploy.tar.gz -C prod f-deploy
+echo "Packing backend"
+tar -zcvf prod/deploy.tar.gz -C prod deploy > /dev/null
+echo "Packing frontend"
+tar -zcvf prod/f-deploy.tar.gz -C prod f-deploy > /dev/null
 
-ansible-playbook ~/ansible/deploy_run.yml -i hosts
+echo "Deploying"
+ansible-playbook ~/ansible/deploy_run.yml -i ~/ansible/hosts
+exit $?
