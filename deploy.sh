ba47b973fd5eb71b6a755dbeaa309d71c0900654
r.ushakov <r.ushakov@xsolla.com>
2022-08-26 21:55:49 +0600

    fix frontend deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ mkdir -p prod/deploy
 mkdir -p prod/f-deploy
 
 mv backend/target/* prod/deploy
-mv frontend/dist prod/f-deploy
+cp -rp frontend/dist/* prod/f-deploy
+rm -rf frontend/dist
 
 rm -rf backend/target
 
