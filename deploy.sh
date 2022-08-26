4b84c7b2777286b83030df0b70386ebf41b188d5
r.ushakov <r.ushakov@xsolla.com>
2022-08-26 20:58:17 +0600

    deploy for runner?????

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,11 @@ tar -zcvf prod/deploy.tar.gz -C prod deploy > /dev/null
 echo "Packing frontend"
 tar -zcvf prod/f-deploy.tar.gz -C prod f-deploy > /dev/null
 
+echo "Copying scripts"
+cp post_deploy_bg.sh prod/post_deploy_bg.sh
+cp swap_release.sh prod/swap_release.sh
+cp notification.sh prod/notification.sh
+
 echo "Deploying"
-ansible-playbook ~/ansible/deploy_run.yml -i ~/ansible/hosts
+ansible-playbook ~/ansible/deploy_run.yml -i ~/ansible/hosts --extra-vars "local_working_directory=$(pwd)"
 exit $?
